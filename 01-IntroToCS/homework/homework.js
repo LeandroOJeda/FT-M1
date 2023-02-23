d7f549f615c4c4a22a37ea807eeb9bcf2f4f4cca
Leandro <leo170994@gmail.com>
2023-02-22 22:16:56 -0300

    First Homework

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -1,8 +1,34 @@
 'use strict';
 
-function BinarioADecimal(num) {}
+function BinarioADecimal(num) {
+      let binario = num.split('');
+      let multiplicador = 1
+      let decimal = []
+   
+      for (let i = binario.length - 1; i >= 0  ; i--) {
+         decimal.push(binario[i] * multiplicador)
+         multiplicador = multiplicador * 2
+      }
+  
+      return decimal.reduce((acum,currentVal) => acum + currentVal)
+}
 
-function DecimalABinario(num) {}
+function DecimalABinario(num) {
+  let resto = num;
+  let binario = [];
+
+  while (resto >= 1) {
+    if (resto % 2) {
+      binario.unshift("1");
+    } else {
+      binario.unshift("0");
+    }
+    resto = Math.floor(resto / 2)
+    
+  }
+
+  return binario.join("");
+}
 
 module.exports = {
    BinarioADecimal,
